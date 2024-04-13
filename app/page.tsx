82890bc9f49bc9ff82fbc3dff7a978e5e287e46f
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-13 14:24:16 +0530

    add signin signup page using clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">Page Form App</div>
+    <div className="flex justify-center items-center h-screen">
+      Page Form App
+      <UserButton/>
+    </div>
   );
 }
